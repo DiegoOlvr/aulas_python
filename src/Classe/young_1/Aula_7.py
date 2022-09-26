ebcf65a9d3e07d4819a8f1bab5c8983de9ac59f2
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2022-09-26 16:51:18 -0300

    Aula 8 - funções

--- src/Classe/young_1/Aula_7.py
+++ src/Classe/young_1/Aula_7.py
@@ -2,28 +2,29 @@
 # ! REVOLVENDO A LISTA DA AULA ANTERIOR
 from operator import itemgetter
 from random import randint
-from time import  sleep
+from time import sleep
 
 
 def exercicio1():
     pessoas = dict()
-    
+
     for n in range(0, 4):
         nome = input(f'Digite o nome da {n+1}ª pessoa: ').strip().capitalize()
         caracteristica = input(f'Digite uma característica do(a) {nome}: ')
         pessoas.update({nome: caracteristica})
-    
+
     print('')
     for aluno in pessoas:
         print(f'{aluno} é {pessoas[aluno]}')
+
 
 def exercicio2():
     numeros = list()
     sem_repeticoes = list()
     conjunto = set()
     while True:
-        n = input(f'Digite um número: [FIM = encerramento] ').upper()
-        
+        n = input('Digite um número: [FIM = encerramento] ').upper()
+
         if n.isnumeric():
             numeros.append(n)
         elif n == 'FIM':
@@ -33,10 +34,11 @@ def exercicio2():
         conjunto.update(n)
         if n not in sem_repeticoes:
             sem_repeticoes.append(n)
-        
+
     print(f'Primeira lista: {numeros}')
     print(f'Segunda lista: {sem_repeticoes}')
     print(f'O conjunto: {conjunto}')
+
 
 def exercicio3():
     jogadores = dict()
@@ -47,7 +49,7 @@ def exercicio3():
         dice = randint(1, 6)
         print(f'{player} tirou {dice}')
         jogadores.update({player: dice})
-    # [[nome, valor], [nome, valor], [nome, valor], [nome, valor], [nome, valor], ]
+# [[nome, valor], [nome, valor], [nome, valor], [nome, valor], [nome, valor], ]
     ranking = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
     print('='*20)
     print(f'{"RANKING":^20}')
@@ -56,12 +58,13 @@ def exercicio3():
         print(f'{i+1}º lugar: {v[0]} com {v[1]}')
         sleep(1)
 
+
 def exercicio6():
     gaiola = set()
     player_cartela = set()
     pc_cartela = set()
     tamanho1 = tamanho2 = cont = 1
-    
+
     while tamanho1 <= 6 and tamanho2 <= 6:
         if len(player_cartela) < 7:
             player_cartela.add(str(randint(1, 60)))
@@ -72,7 +75,7 @@ def exercicio6():
     print(f'Sua cartela: {player_cartela}\nCartela do PC: {pc_cartela}')
     for num in range(1, 61):
         gaiola.add(str(num))
-    
+
     while True:
         sortear = gaiola.pop()
         print(f'O número sorteado foi: {sortear}')
@@ -80,24 +83,22 @@ def exercicio6():
         cont += 1
         if sortear in player_cartela:
             player_cartela.remove(sortear)
-            print(f'\nPlayer: aeeee')
+            print('\nPlayer: aeeee')
             tamanho1 -= 1
             print(tamanho1)
             if tamanho1 == 1:
                 print(f'Cartela perdedora{pc_cartela}')
-                print(f'\nPlayer: ', end=' ')
+                print('\nPlayer: ', end=' ')
                 break
-        
+
         pc_cartela.discard(sortear)
         if len(pc_cartela) <= 0:
             print(f'Cartela perdedora{player_cartela}')
-            print(f'\nPC: ', end=' ')
+            print('\nPC: ', end=' ')
             break
-            
+
     print('BINGOOOOOO!!!')
     print(f'Tivemos {cont} rodadas')
-    
-
 
 
 # ! FUNÇÕES
@@ -105,16 +106,16 @@ def exercicio6():
 Um conjunto de algoritmos que serão executados
 print() => mostra algo no terminal ou output (saida)
 len()
-max(), min(), strip(), 
+max(), min(), strip(),
 função é como se fosse uma variável
 ela armazena comandos
 
-toda vez que formos usar uma função, 
+toda vez que formos usar uma função,
 precisa por os parenteses
 nome_da_função()
 
-para criar as nossas funções 
-def = definir, nome da função, () e 
+para criar as nossas funções
+def = definir, nome da função, () e
 dentro deles pode ter algo ou não
 def nome_da_função(parametros*):
 
--- src/Classe/young_1/Aula_8.py
+++ src/Classe/young_1/Aula_8.py
@@ -4,16 +4,16 @@
 Um conjunto de algoritmos que serão executados
 print() => mostra algo no terminal ou output (saida)
 len()
-max(), min(), strip(), 
+max(), min(), strip(),
 função é como se fosse uma variável
 ela armazena comandos
 
-toda vez que formos usar uma função, 
+toda vez que formos usar uma função,
 precisa por os parenteses
 nome_da_função()
 
-para criar as nossas funções 
-def = definir, nome da função, () e 
+para criar as nossas funções
+def = definir, nome da função, () e
 dentro deles pode ter algo ou não
 def nome_da_função(parametros*):
 
@@ -21,11 +21,13 @@ def nome_da_função(parametros*):
 
 # parametros / argumentos
 
-def mult(n1: int, n2: int, n3:int, n4:int):
+
+def mult(n1: int, n2: int, n3: int, n4: int):
     resultado = int(n1) * int(n2)
     div = int(n3) / int(n4)
-    fim  = resultado + div
+    fim = resultado + div
     return fim
+
 
 # Argumentos posicionais
 print(f'primeiro teste: {mult(3, 2, 2, 4)}')
@@ -41,7 +43,7 @@ print(f'Terceiro teste: {mult(2, 2, n4=3, n3=4)}')
 #     itens.append(ing2)
 #     itens.append(ing3)
 #     itens.append(ing4)
-#     return itens 
+#     return itens
 #     print('pudim')
 
 # ingredientes = list()
@@ -80,20 +82,24 @@ preparar_acai('Granola', 'Banana', 'Paçoca', 'Morango', 'leite em pó')
 
 # # **kwagrs = argumentos multiplos / nomeados key words (palavras chaves)
 
+
 def preparar_acai2(**ingredientes):
     print('O seu Açai_2 contém: ')
     for item in ingredientes:
         print(ingredientes[item])
     return ingredientes
 
-preparo = preparar_acai2(ing1 = 'Banana', ing2 = 'Granola', ing3 = 'Morango')
+
+preparo = preparar_acai2(ing1='Banana', ing2='Granola', ing3='Morango')
 print(preparo)
 print(type(preparo))
 
-# * RECURSIVIDADE
+# * RECURSIVIDADEk
 # fibonacci, fatorial
 # função sem recursividade
-def fatorial(num: int): # 5! = 5 x 4 x 3 x 2 x 1 x 0 == 120
+
+
+def fatorial(num: int):  # 5! = 5 x 4 x 3 x 2 x 1 x 0 == 120
     if num < 0:
         print('Não existe fatorial de 0')
     elif num == 0:
@@ -101,9 +107,10 @@ def fatorial(num: int): # 5! = 5 x 4 x 3 x 2 x 1 x 0 == 120
     else:
         resul = 1
         while (num > 1):
-            resul *= num # resul = resul * num
+            resul *= num  # resul = resul * num
             num -= 1     # num = num - 1
         return resul
+
 
 print(fatorial(5))
 # função com recursividade
