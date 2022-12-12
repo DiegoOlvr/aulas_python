7c5496711153be5987cf9bd0deb946ce9d703d4a
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2022-12-12 20:52:59 -0300

    Adicionando todas os cod de moedas

--- src/Classe/young_1/Aula_18/tela_cotacao.py
+++ src/Classe/young_1/Aula_18/tela_cotacao.py
@@ -2,15 +2,23 @@ import PySimpleGUI as psg
 from main import pegar_cotacao
 
 moedas = [
-    'AFN',
-    'MGA',
-    'THB',
-    'PAB',
-    'ETB',
-    'BTC',
-    'BOB',
-    'VEF',
-    'XBR', 'GHS', 'SVC', 'USD', 'AUD', 'CAD', 'ETH', 'EUR', 'CHF']
+    'AFN', 'MGA', 'THB', 'PAB', 'ETB', 'BTC', 'BOB', 'VEF', 'XBR', 'GHS',
+    'SVC', 'CRC', 'CZK', 'DKK', 'ISK', 'NOK', 'SEK', 'NIO', 'SDR', 'GMD',
+    'MKD', 'DZD', 'IQD', 'JOD', 'KWD', 'LYD', 'RSD', 'TND', 'BHD', 'MAD',
+    'AED', 'STD', 'DOGE', 'VND', 'AMD', 'USD', 'AUD', 'CAD', 'JMD', 'NAD',
+    'NZD', 'TWD', 'ZWL', 'BSD', 'KYD', 'BBD',  'BZD', 'BND', 'SGD', 'FJD',
+    'HKD', 'TTD', 'XCD', 'CVE', 'ETH', 'EUR', 'HUF', 'BIF', 'XAF', 'XOF',
+    'XPF', 'KMF', 'RWF', 'CHF', 'CHFRTS', 'GNF', 'DJF', 'HTG', 'PYG',
+    'ANG', 'UAH', 'JPY', 'JPYRTS', 'PGK', 'LAK', 'HRK', 'MWK', 'ZMK',
+    'AOA', 'MMK', 'GEL', 'ALL', 'HNL', 'MDL', 'RON', 'BGN', 'EGP', 'GBP',
+    'LBP', 'SDG', 'SYP', 'SZL', 'LTC', 'LSL', 'AZN', 'BAM', 'MZN', 'MNT',
+    'NGNPARALLEL', 'NGN', 'NGNI', 'TRY', 'ILS', 'MRO', 'MOP', 'ARS', 'CLP',
+    'COP', 'CUP', 'DOP', 'PHP', 'MXN', 'UYU', 'BWP', 'GTQ', 'ZAR', 'BRL',
+    'BRLT', 'QAR', 'IRR', 'OMR', 'KHR', 'MYR', 'YER', 'SAR', 'BYN', 'RUBTOM',
+    'RUBTOD', 'RUB', 'MVR', 'IDR', 'INR', 'MUR', 'NPR', 'PKR', 'LKR', 'SCR',
+    'KES', 'SOS', 'TZS', 'UGX', 'PEN', 'KGS', 'UZS', 'TJS', 'TMT', 'BDT',
+    'KZT', 'VUV', 'KRW', 'XAGG', 'XRP', 'CNY', 'CNH', 'PLN'
+]
 
 # Escolhendo o tema
 psg.theme('DarkPurple6')
